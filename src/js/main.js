a219795607548114c841e92f082bc135c2c139ee
NeverMind <Fry_Vetoshkin_97@ukr.net>
2019-03-17 14:51:32 +0200

    add characteristics block on goods page

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,13 @@
 */
 //= partials/app.js
 //
+$(document).ready(function() {
+    $('.js-title').on('click',function() {
+        var container = $('.js-container');
+        var itemNumber = $(this).data('item');
+        $('.js-title').removeClass('active');
+        $(this).addClass('active');
+        container.removeClass('active');
+        $('.js-container:eq(' + itemNumber + ')').addClass('active');
+    });
+});
